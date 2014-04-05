6df615fd14dedb4736ec8387e873c216482b0163
Elitward.Dunn <elitward@gmail.com>
2014-04-05 16:09:48 -0700

    fist commit

---
+++ src/javafxapplication1/NewClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javafxapplication1;
+
+/**
+ *
+ * @author Elitward
+ */
+public class NewClass {
+    private int member1;
+    private boolean member2;
+
+    /**
+     * @return the member1
+     */
+    public int getMember1() {
+        return member1;
+    }
+
+    /**
+     * @param member1 the member1 to set
+     */
+    public void setMember1(int member1) {
+        this.member1 = member1;
+    }
+}
